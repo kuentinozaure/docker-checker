a8e5d95ce168b6d77d00f36bbabbb57ad257a495
lil_kent1 <aubryquentin700@gmail.com>
2021-08-20 00:11:56 +0200

    add: util element
    - tool.js (element can help during development)

---
+++ util/tool.js
@@ -0,0 +1,28 @@
+const DockerInfo = require("../model/docker-info");
+
+/**
+ * This method can generate a list of docker container
+ * @param {*} dockerCommandResult is the result of container cli
+ * @returns a list of object with docker info
+ */
+function getDockerContainer(dockerCommandResult) {
+  let array_of_container = dockerCommandResult.split(`\n`);
+  // remove the first line with the name of column
+  array_of_container.shift();
+  // remove the last line with the empty line
+  array_of_container.pop();
+
+  array_of_container = array_of_container.map((value) => {
+    return new DockerInfo(
+      value.split("   ")[0],
+      value.split("   ")[4].split(" ")[0],
+      value.split("   ")[1],
+      value.split("   ")[3],
+      value.split("   ")[8].replace(" ", "")
+    );
+  });
+
+  return array_of_container;
+}
+
+module.exports.getDockerContainer = getDockerContainer;
